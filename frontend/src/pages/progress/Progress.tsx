fe17097708ec37ec2e7b0de899098e7d17e07497
Pooja Jain <118278785+pjn93@users.noreply.github.com>
2025-04-30 04:55:14 +0530

    Update Progress.tsx

--- frontend/src/pages/progress/Progress.tsx
+++ frontend/src/pages/progress/Progress.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 function Progress() {
   return (
-    <div>
-     Progress
+    <div className="dashboard">
+    <div className="heading">
+      <h2>Progress Reports</h2>
+      <p>Easy: 36%</p>
+      <p>Medium: 41%</p>
+      <p>Hard: 41%</p>
     </div>
+    </div>
+
   )
 }
 
